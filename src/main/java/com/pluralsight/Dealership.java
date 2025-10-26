b919feda20a0120d0cb792e484f0a7e4ba35fa14
RICHIE-YIN <richiesyin@icloud.com>
2025-10-26 14:50:16 -0700

    First commit

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,89 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        if(vehicle != null) {
+            inventory.add(vehicle);
+        }
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return new ArrayList<Vehicle>(inventory);
+    }
+
+    public List<Vehicle> getVehiclesByPrice(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int minYear, int maxYear) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int minMiles, int maxMiles) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Vehicle> inventory) {
+        this.inventory = inventory;
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/git/ignore/throwawaycode.java
@@ -0,0 +1,78 @@
+package com.pluralsight.git.ignore;
+
+import com.pluralsight.Vehicle;
+
+public class throwawaycode {
+//    public void getVehiclesByPrice(int min, int max) {
+//        for(Vehicle v : inventory) {
+//            if(v.getPrice() >= min && v.getPrice() <= max) {
+//                System.out.println("Vehicle found at: " + name);
+//                System.out.printf("vin: %d, year: %d, make: %s, model: %s, vehicleType: %s, color: %s, odometer: %d, price: .%2f", v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+//            } else {
+//                System.out.println("No vehicles found in this price range.");
+//            }
+//        }
+//    }
+//
+//    public void getVehiclesByMakeModel(String make, String model) {
+//        for(Vehicle v : inventory) {
+//            if(v.getMake().equalsIgnoreCase(make) && v.getModel().equalsIgnoreCase(model)) {
+//                System.out.println("Vehicle found at: " + name);
+//                System.out.printf("vin: %d, year: %d, make: %s, model: %s, vehicleType: %s, color: %s, odometer: %d, price: .%2f", v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+//            } else {
+//                System.out.println("No vehicles found with this make and model.");
+//            }
+//        }
+//    }
+//
+//    public void getVehiclesByYear(int min, int max) {
+//        for(Vehicle v : inventory) {
+//            if(v.getYear() >= min && v.getYear() <= max) {
+//                System.out.println("Vehicle found at: " + name);
+//                System.out.printf("vin: %d, year: %d, make: %s, model: %s, vehicleType: %s, color: %s, odometer: %d, price: .%2f", v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+//            } else {
+//                System.out.println("No vehicles found in this year range.");
+//            }
+//        }
+//    }
+//
+//    public void getVehiclesByColor(String color) {
+//        for(Vehicle v : inventory) {
+//            if(v.getColor().equalsIgnoreCase(color)) {
+//                System.out.println("Vehicle found at: " + name);
+//                System.out.printf("vin: %d, year: %d, make: %s, model: %s, vehicleType: %s, color: %s, odometer: %d, price: .%2f", v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+//            } else {
+//                System.out.println("No vehicles found with this color.");
+//            }
+//        }
+//    }
+//
+//    public void getVehiclesByMileage(int min, int max) {
+//        for(Vehicle v : inventory) {
+//            if(v.getOdometer() >= min && v.getOdometer() <= max) {
+//                System.out.println("Vehicle found at: " + name);
+//                System.out.printf("vin: %d, year: %d, make: %s, model: %s, vehicleType: %s, color: %s, odometer: %d, price: .%2f", v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+//            } else {
+//                System.out.println("No vehicles found within this mileage range.");
+//            }
+//        }
+//    }
+//
+//    public void getVehiclesByType(String type) {
+//        for(Vehicle v : inventory) {
+//            if(v.getColor().equalsIgnoreCase(type)) {
+//                System.out.println("Vehicle found at: " + name);
+//                System.out.printf("vin: %d, year: %d, make: %s, model: %s, vehicleType: %s, color: %s, odometer: %d, price: .%2f", v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+//            } else {
+//                System.out.println("No vehicles with this type found..");
+//            }
+//        }
+//    }
+//
+//    public void getAllVehicles() {
+//        for(Vehicle v : inventory) {
+//            System.out.println("Dealership: " + name);
+//            System.out.printf("vin: %d, year: %d, make: %s, model: %s, vehicleType: %s, color: %s, odometer: %d, price: .%2f", v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+//        }
+//    }
+}
